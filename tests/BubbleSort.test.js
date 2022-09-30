8bbb3263dc8f41576911b3f60100a80ef39c83e0
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-30 14:27:46 -0500

    test sort by firstName and lastName

--- tests/BubbleSort.test.js
+++ tests/BubbleSort.test.js
@@ -1,6 +1,11 @@
 import BubbleSort from '../src/BubbleSort/BubbleSort';
 import BubbleSortObjArr from '../src/BubbleSort/BubbleSortObjArr';
-import { employees, sortedEmployeesByAge } from '../src/data';
+import {
+  employees,
+  sortedEmployeesByAge,
+  sortedEmployeesByFirstName,
+  sortedEmployeesByLastName,
+} from '../src/data';
 
 test('should return [1,2,3,4,5,6,7,8,9,10]', () => {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
@@ -8,7 +13,17 @@ test('should return [1,2,3,4,5,6,7,8,9,10]', () => {
   expect(sortedNumber).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
 
-test('should return an ordered array of objects', () => {
+test('should return an ordered array of objects by age', () => {
   const sorted = BubbleSortObjArr(employees, 'age');
   expect(sorted).toEqual(sortedEmployeesByAge);
+});
+
+test('should return and orderd array of objects by first name', () => {
+  const sorted = BubbleSortObjArr(employees, 'firstName');
+  expect(sorted).toEqual(sortedEmployeesByFirstName);
+});
+
+test('should return and orderd array of objects by last name', () => {
+  const sorted = BubbleSortObjArr(employees, 'lastName');
+  expect(sorted).toEqual(sortedEmployeesByLastName);
 });
