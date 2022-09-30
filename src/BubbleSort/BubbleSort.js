25dcae7c935cd3c9460e78e08584804e72a1d6b9
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-30 13:37:06 -0500

    add Bubblesort

---
+++ src/BubbleSort/BubbleSort.js
@@ -0,0 +1,17 @@
+const BubbleSort = (arr) => {
+  let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+
+export default BubbleSort;
